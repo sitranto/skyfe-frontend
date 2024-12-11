9e8cc858d1af1f43e593d79f722655c7d8916c84
oleg <oleg_zub00@mail.ru>
2024-12-11 22:10:16 +0300

    form center

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,39 @@
 <template>
-  <div>
-    <div class="logo">
-      <img class="d-block mx-auto" src="../static/fullLogo.svg" alt="#"/>
-    </div>
-    <div>
-      <v-text-field class="custom-input"
-                    label="login"
-                    type="text"
-                    hide-details
-                    outlined/>
-    </div>
+  <div class="auth d-flex align-center justify-center">
+    <div class="auth-wrapper">
 
-    <div class="mt-8">
-      <v-text-field class="custom-input"
-                    label="password"
-                    type="password"
-                    hide-details
-                    outlined/>
+      <div class="logo">
+        <img class="d-block mx-auto" src="../static/fullLogo.svg" alt="#"/>
+      </div>
+      <div class="mt-15">
+        <v-text-field class="custom-input"
+                      label="login"
+                      type="text"
+                      hide-details
+                      outlined/>
+      </div>
+
+      <div class="mt-8">
+        <v-text-field class="custom-input"
+                      label="password"
+                      type="password"
+                      hide-details
+                      outlined/>
+      </div>
+
+      <div class="auth-actions">
+        <div>
+          <v-btn> asfasfa </v-btn>
+        </div>
+
+        <div>
+          <v-btn> asfasfa </v-btn>
+        </div>
+
+      </div>
+
+
+
     </div>
   </div>
 </template>
@@ -27,3 +44,18 @@ import {Vue, Component} from 'vue-property-decorator';
 export default class Index extends Vue {
 }
 </script>
+
+<style>
+.auth {
+  width: 100%;
+  height: 100vh;
+}
+
+.auth-wrapper {
+  min-width: 700px;
+}
+
+.auth-actions {
+  margin-top: 87px;
+}
+</style>
