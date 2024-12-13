cb00c633035f3261df31cc744549e859ef8ec6ef
Arsehha <159578130+Arsehha@users.noreply.github.com>
2024-12-13 20:47:26 +0300

    request 0.1

--- pages/Auth/login.vue
+++ pages/Auth/login.vue
@@ -2,9 +2,11 @@
   <div>
     <div class="mt-15">
       <v-text-field class="custom-input"
-                    label="Login"
+                    label="Phone"
                     type="text"
                     hide-details
+                    v-model="model.phone"
+                    v-mask="`+7##########`"
                     outlined/>
     </div>
 
@@ -17,10 +19,9 @@
                     :append-icon="showPass.one ? 'mdi-eye' : 'mdi-eye-off'"
                     :type="showPass.one ? 'text' : 'password'"
                     @click:append="showPass.one = !showPass.one"
-
+                    v-model="model.password"
                     outlined/>
     </div>
-
     <div class="auth-actions d-flex align-center justify-center flex-column">
       <div>
         <v-btn
@@ -29,6 +30,7 @@
           color="#8A138C"
           width="150px"
           height="40px"
+          @click.prevent="auth"
         >Войти
         </v-btn>
       </div>
@@ -41,15 +43,44 @@
   </div>
 </template>
 <script lang="ts">
-import {Vue, Component} from 'vue-property-decorator';
+import {Vue, Component, Watch} from 'vue-property-decorator';
+import * as string_decoder from "node:string_decoder";
 
 @Component({
   layout: "auth"
 })
 export default class login extends Vue {
-showPass: any = {
-  one: false,
-}
+  showPass: any = {
+    one: false,
+  }
+
+  model: any = {
+    phone: "+7",
+    password: ""
+  }
+
+  async auth() {
+    if (this.model.phone.length != 12) {
+      return
+    }
+    if (this.model.password.length >= 8) {
+      return
+    }
+
+    await this.$axios.post("/auth", this.model, {})
+      .then((response) = {
+
+      })
+  }
+
+  @Watch("model.phone")
+  changeModelPhone() {
+    console.log(this.model.phone)
+    if (this.model.phone.length <= 2) {
+      return this.model.phone = "+7"
+    }
+  }
+
 }
 </script>
 <style scoped>
