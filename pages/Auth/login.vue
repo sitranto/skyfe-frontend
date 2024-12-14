196a0996571f7478b5c06eb00d92972710946d7d
oleg <oleg_zub00@mail.ru>
2024-12-14 15:10:23 +0300

    reg layout 1.0

--- pages/Auth/login.vue
+++ pages/Auth/login.vue
@@ -1,46 +1,53 @@
 <template>
-  <div>
-    <div class="mt-15">
-      <v-text-field class="custom-input"
-                    label="Phone"
-                    type="text"
-                    hide-details
-                    v-model="model.phone"
-                    v-mask="`+7##########`"
-                    outlined/>
-    </div>
+  <!-- делаю обёрточный тэг v-form, чтобы накинуть валидацию -->
+  <v-form v-model="validForm" ref="validForm">
+    <v-card class="ma-0 mt-15 pa-0"
+            style="background-color: transparent !important;"
+            :disabled="loading"
+            :loading="loading"
+            elevation="0">
+      <div class="mt-4">
+        <v-text-field class="custom-input"
+                      label="Phone"
+                      type="text"
+                      v-model="model.phone"
+                      v-mask="`+7##########`"
+                      :rules="[rules.required, rules.length(12)]"
+                      outlined/>
+      </div>
 
-    <div class="mt-8">
-      <v-text-field class="custom-input"
-                    label="Password"
-                    type="password"
-                    hide-details
-                    color="#8A138C"
-                    :append-icon="showPass.one ? 'mdi-eye' : 'mdi-eye-off'"
-                    :type="showPass.one ? 'text' : 'password'"
-                    @click:append="showPass.one = !showPass.one"
-                    v-model="model.password"
-                    outlined/>
-    </div>
-    <div class="auth-actions d-flex align-center justify-center flex-column">
       <div>
-        <v-btn
-          class="text-none auth-button--outlined"
-          outlined
-          color="#8A138C"
-          width="150px"
-          height="40px"
-          @click.prevent="auth"
-        >Войти
-        </v-btn>
+        <v-text-field class="custom-input"
+                      label="Password"
+                      type="password"
+                      color="#8A138C"
+                      :rules="[rules.required, rules.password]"
+                      :append-icon="showPass ? 'mdi-eye' : 'mdi-eye-off'"
+                      :type="showPass ? 'text' : 'password'"
+                      @click:append="showPass = !showPass"
+                      v-model="model.password"
+                      outlined/>
       </div>
+      <div class="auth-actions d-flex align-center justify-center flex-column">
+        <div>
+          <v-btn
+            class="text-none auth-button--outlined"
+            outlined
+            color="#8A138C"
+            width="150px"
+            height="40px"
+            @click.prevent="auth"
+          >Войти
+          </v-btn>
+        </div>
 
-      <div class="mt-2">
-        <a class="auth-button--text" href="#" @click.prevent="$router.push(`/auth/reg`)">Создать аккаунт</a>
+        <div class="mt-2">
+          <a class="auth-button--text" href="#" @click.prevent="$router.push(`/auth/reg`)">Создать аккаунт</a>
+        </div>
+
       </div>
-
-    </div>
-  </div>
+    </v-card>
+  </v-form>
 </template>
 <script lang="ts">
 import {Vue, Component, Watch} from 'vue-property-decorator';
@@ -50,27 +57,56 @@ import * as string_decoder from "node:string_decoder";
   layout: "auth"
 })
 export default class login extends Vue {
-  showPass: any = {
-    one: false,
-  }
+  validForm: boolean = false;
+  loading: boolean = false;
+  showPass: boolean = false;
 
   model: any = {
     phone: "+7",
     password: ""
   }
 
-  async auth() {
-    if (this.model.number.length != 12) {
-      return
-    }
-    if (this.model.password.length < 8) {
-      return
-    }
-    if (this.model.password.length > 32) {
-      return
-    }
+  /* rules отвечает за то, как правильно вбиты поля, исходя из них, форма становится валидной либо же нет */
+  rules = {
+    length: (len: any) => (v: any) =>
+      (v || '').length >= (len ?? 8) ||
+      `Недопустимая длина символов`,
+    password: (v: any) =>
+      !!(v || '').match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*(_|[^\w])).+$/) ||
+      'Пароль должен содержать заглавную букву, цифру и специальный символ.',
+    required: (v: any) => !!v || 'Это поле обязательно к заполнению',
+  }
 
-    await this.$axios.post("http://localhost:8080/auth", this.model, {})
+  async auth() {
+    // перевёл все условия на rules
+    // подробнее как работает, смотри по ссылке ниже
+    // https://v2.vuetifyjs.com/en/components/text-fields/#custom-validation
+    // и тут
+    // https://v2.vuetifyjs.com/en/components/forms/#rules
+
+
+    // if (this.model.number.length != 12) {
+    //   return
+    // }
+    // if (this.model.password.length < 8) {
+    //   return
+    // }
+    // if (this.model.password.length > 32) {
+    //   return
+    // }
+
+
+    this.loading = true;
+
+    // По нажатию кнопки логина, мы валидируем форму,
+    // Это сделано для того, чтобы пользователь не смог пустую форму отправить на логин
+    // Если валидация успешна, мы пропускаем дальше, нет, ну иди заполняй форму
+    this.validateForm && await this.$axios.post("http://localhost:8080/auth", this.model, {})
+      // Уточни, включен ли у него cors?
+      // Иначе будет выдавать ошибку при запросе!
+      // Если нет, то обрати внимание, как у него идут запросы.
+      // Если у него запросы например http://localhost:8080/api/auth - имеют /api/
+      // Проще будет сделать проксирование на запросы /api, и они сократятся до /api/auth, вместо полной строчки http...
       .then((response) => {
         const jwt = response.data.access_token;
         if (jwt) {
@@ -79,6 +115,12 @@ export default class login extends Vue {
           this.$router.push("../index.vue");
         }
       })
+      .finally(() => {
+        this.loading = false;
+      })
+
+    // На случай, если запрос не сработает, мы всё равно включаем форму обратно
+    this.loading = false;
   }
 
   @Watch("model.phone")
@@ -89,6 +131,10 @@ export default class login extends Vue {
     }
   }
 
+  get validateForm() {
+    // @ts-ignore
+    return this.$refs.validForm.validate()
+  }
 }
 </script>
 <style scoped>
--- pages/Auth/reg.vue
+++ pages/Auth/reg.vue
@@ -9,151 +9,249 @@
       <v-stepper v-model="stepper" elevation="0"
                  style="background-color: transparent !important;">
 
-        <v-stepper-items>
-          <v-stepper-content :step="1" class="ma-0 pa-0">
+        <v-form v-model="validForm.one" ref="validFormOne">
+          <v-stepper-items>
+            <v-stepper-content :step="1" class="ma-0 pa-0">
 
-            <div class="mt-5">
-              <v-text-field class="custom-input"
-                            label="Phone"
-                            type="text"
-                            hide-details
-                            outlined/>
-            </div>
+              <div class="mt-5">
+                <v-text-field class="custom-input"
+                              v-mask="`+7##########`"
+                              v-model="model.phone"
+                              label="Phone"
+                              type="text"
+                              :rules="[rules.required, rules.length(12)]"
+                              outlined/>
+              </div>
 
-            <v-btn
-              @click="checkPhoneAndCont"
-              class="d-block mx-auto text-none auth-button--outlined mt-5"
-              outlined
-              color="#8A138C"
-              height="40px">
-              Проверка телефона
-            </v-btn>
+              <v-card-actions class="d-flex justify-center">
+                <v-btn
+                  @click="checkPhoneAndCont"
+                  class="d-block text-none auth-button--outlined mt-5 px-7"
+                  outlined
+                  color="#8A138C"
+                  height="45px">
+                  Проверка телефона
+                </v-btn>
+              </v-card-actions>
 
-          </v-stepper-content>
-        </v-stepper-items>
+            </v-stepper-content>
+          </v-stepper-items>
+        </v-form>
 
+        <v-form v-model="validForm.two" ref="validFormTwo">
+          <v-stepper-items>
+            <v-stepper-content :step="2">
 
-        <v-stepper-items>
-          <v-stepper-content :step="2">
+              <div class="mt-5">
+                <v-text-field class="custom-input"
+                              label="First name"
+                              v-model="model.firstname"
+                              :rules="[rules.required]"
+                              type="text"
+                              outlined/>
+              </div>
 
-            <div class="mt-5">
-              <v-text-field class="custom-input"
-                            label="First name"
-                            type="text"
-                            hide-details
-                            outlined/>
-            </div>
+              <div>
+                <v-text-field class="custom-input"
+                              label="Second name"
+                              v-model="model.secondname"
+                              :rules="[rules.required]"
+                              type="text"
+                              outlined/>
+              </div>
 
-            <div class="mt-5">
-              <v-text-field class="custom-input"
-                            label="Second name"
-                            type="text"
-                            hide-details
-                            outlined/>
-            </div>
-
-            <div class="mt-5">
-              <v-text-field class="custom-input"
-                            label="Username"
-                            type="text"
-                            hide-details
-                            outlined/>
-            </div>
-
-
-            <v-btn
-              @click="nextStepButton"
-              class="d-block mx-auto text-none auth-button--outlined mt-5"
-              outlined
-              color="#8A138C"
-              height="40px">
-              Далее
-            </v-btn>
-
-          </v-stepper-content>
-        </v-stepper-items>
-
-        <v-stepper-items>
-          <v-stepper-content :step="3">
-
-            <div class="mt-5">
-              <v-text-field class="custom-input"
-                            label="Password"
-                            type="password"
-                            hide-details
-                            color="#8A138C"
-                            :append-icon="showPass.one ? 'mdi-eye' : 'mdi-eye-off'"
-                            :type="showPass.one ? 'text' : 'password'"
-                            @click:append="showPass.one = !showPass.one"
-
-                            outlined/>
-            </div>
-
-            <div class="mt-5">
-              <v-text-field class="custom-input"
-                            label="Repeat password"
-                            type="password"
-                            hide-details
-                            color="#8A138C"
-                            :append-icon="showPass.two ? 'mdi-eye' : 'mdi-eye-off'"
-                            :type="showPass.two ? 'text' : 'password'"
-                            @click:append="showPass.two = !showPass.two"
-                            outlined/>
-            </div>
-
-            <v-btn
-              @click="registration"
-              class="d-block mx-auto text-none auth-button--outlined mt-5"
-              outlined
-              color="#8A138C"
-              height="40px">
-              Регистрация
-            </v-btn>
+              <div>
+                <v-text-field class="custom-input"
+                              label="Username"
+                              v-model="model.username"
+                              :rules="[rules.required]"
+                              type="text"
+                              outlined/>
+              </div>
 
 
-          </v-stepper-content>
-        </v-stepper-items>
+              <v-card-actions class="d-flex justify-center">
+                <v-btn
+                  @click="stepper--"
+                  class="d-block text-none auth-button--outlined mt-5 px-7"
+                  outlined
+                  color="#8A138C"
+                  height="45px">
+                  Назад
+                </v-btn>
 
+                <v-btn
+                  @click="checkUserNameAndCont"
+                  class="d-block text-none auth-button--outlined mt-5 px-7"
+                  outlined
+                  color="#8A138C"
+                  height="45px">
+                  Далее
+                </v-btn>
+              </v-card-actions>
+
+            </v-stepper-content>
+          </v-stepper-items>
+        </v-form>
+
+        <v-form v-model="validForm.three" ref="validFormThree">
+          <v-stepper-items>
+            <v-stepper-content :step="3">
+
+              <div class="mt-5">
+                <v-text-field class="custom-input"
+                              label="Password"
+                              type="password"
+                              color="#8A138C"
+                              v-model="model.password"
+                              :rules="[rules.required, rules.password]"
+                              :append-icon="showPass.one ? 'mdi-eye' : 'mdi-eye-off'"
+                              :type="showPass.one ? 'text' : 'password'"
+                              @click:append="showPass.one = !showPass.one"
+                              outlined/>
+              </div>
+
+              <div class="mt-5">
+                <v-text-field class="custom-input"
+                              label="Repeat password"
+                              type="password"
+                              color="#8A138C"
+                              v-model="passRepeat"
+                              :rules="[rules.required, rules.password, rules.match(model.password)]"
+                              :append-icon="showPass.two ? 'mdi-eye' : 'mdi-eye-off'"
+                              :type="showPass.two ? 'text' : 'password'"
+                              @click:append="showPass.two = !showPass.two"
+                              outlined/>
+              </div>
+
+              <v-card-actions class="d-flex justify-center">
+                <v-btn
+                  @click="registration"
+                  class="d-block text-none auth-button--outlined mt-5 px-7"
+                  outlined
+                  color="#8A138C"
+                  height="45px">
+                  Регистрация
+                </v-btn>
+              </v-card-actions>
+
+            </v-stepper-content>
+          </v-stepper-items>
+        </v-form>
 
       </v-stepper>
     </v-card>
 
-
-    <div class="auth-actions d-flex align-center justify-center flex-column">
+    <div class="auth-actions isRegPage d-flex align-center justify-center flex-column">
       <div>
         <a class="auth-button--text" href="#" @click.prevent="$router.push(`/auth/login`)">Войти</a>
       </div>
     </div>
-
   </div>
 </template>
 <script lang="ts">
-import {Vue, Component} from 'vue-property-decorator';
+import {Vue, Component, Watch} from 'vue-property-decorator';
 
 @Component({
   layout: "auth"
 })
 export default class reg extends Vue {
+  model: any = {
+    phone: "+7",
+    firstname: "",
+    secondname: "",
+    username: "",
+    password: ""
+  }
+
+  passRepeat: string = ""
+
   showPass: any = {
     one: false,
     two: false,
   }
 
+  validForm: any = {
+    one: false,
+    two: false,
+    three: false,
+  };
+
   stepLoading: boolean = false
   stepper: number = 1
 
+
+  rules = {
+    match: (match: any) => (v: any) =>
+      (!!v && v) === match || "Пароли должны совпадать",
+
+    length: (len: any) => (v: any) =>
+      (v || "").length >= (len ?? 8) ||
+      `Недопустимая длина символов`,
+
+    maxValue: (len: any) => (v: any) =>
+      (v || "") < len ||
+      `Недопустимое значение, максимальное значение - ${len}`,
+
+    password: (v: any) =>
+      !!(v || "").match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*(_|[^\w])).+$/) ||
+      "Пароль должен содержать заглавную букву, цифру и специальный символ.",
+
+    required: (v: any) => !!v || "Это поле обязательно к заполнению"
+  };
+
   checkPhoneAndCont() {
-    this.stepper++
+    this.stepLoading = true
+
+    setTimeout(() => {
+      this.validateForm('validFormOne')
+      this.stepLoading = false
+    }, 1200)
+
+    setTimeout(() => {
+      if (this.validateForm('validFormOne')) this.nextStep()
+    }, 1000)
+
   }
 
-  nextStepButton() {
-    this.stepper++
+  checkUserNameAndCont() {
+    this.stepLoading = true
+
+    setTimeout(() => {
+      this.validateForm('validFormTwo')
+      this.stepLoading = false
+    }, 1200)
+
+    setTimeout(() => {
+      if (this.validateForm('validFormTwo')) this.nextStep()
+    }, 1000)
   }
 
   registration() {
+    this.stepLoading = true
+
+    setTimeout(() => {
+      this.validateForm('validFormThree')
+      this.stepLoading = false
+    }, 1200)
 
   }
 
+  nextStep() {
+    return this.stepper++;
+  }
+
+  @Watch("model.phone")
+  changeModelPhone() {
+    if (this.model.phone.length <= 2) {
+      return this.model.phone = "+7"
+    }
+  }
+
+  validateForm(val: string): boolean {
+    // @ts-ignore
+    return this.$refs[val].validate()
+  }
 }
 </script>
 <style scoped>
